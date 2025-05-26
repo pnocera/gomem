1f6cb375804735ecb10c0befeffb740cb542ff66
Pierre Nocera <pnocera@gmail.com>
2025-05-26 17:29:50 +0200

    otw

--- go.mod
+++ go.mod
@@ -9,12 +9,19 @@ require (
 )
 
 require (
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/nats-io/nkeys v0.4.11 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+)
+
+require (
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	golang.org/x/crypto v0.33.0 // indirect
+	github.com/nats-io/nats.go v1.42.0
+	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/net v0.34.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 )
---
+++ pkg/nats/client.go
@@ -0,0 +1,43 @@
+package nats
+
+import (
+	"log"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+// Connect establishes a connection to a NATS server.
+// It takes the NATS server URL and a variadic slice of nats.Option as arguments.
+// This allows for flexible configuration (credentials, connection name, etc.).
+func Connect(natsURL string, opts ...nats.Option) (*nats.Conn, error) {
+	// Apply default options first, then allow user-provided options to override or add.
+	defaultOpts := []nats.Option{
+		nats.Name("Gomem NATS Client"), // You might want to make the client name configurable
+		nats.Timeout(10 * time.Second),
+		nats.RetryOnFailedConnect(true),
+		nats.MaxReconnects(10), // Increased max reconnects
+		nats.ReconnectWait(3 * time.Second),
+		nats.DisconnectErrHandler(func(nc *nats.Conn, err error) {
+			log.Printf("NATS client disconnected. Last error: %v", err)
+		}),
+		nats.ReconnectHandler(func(nc *nats.Conn) {
+			log.Printf("NATS client reconnected to %s", nc.ConnectedUrl())
+		}),
+		nats.ClosedHandler(func(nc *nats.Conn) {
+			log.Println("NATS client connection closed.")
+		}),
+	}
+
+	finalOpts := append(defaultOpts, opts...)
+
+	// Connect to NATS
+	nc, err := nats.Connect(natsURL, finalOpts...)
+	if err != nil {
+		log.Printf("Error connecting to NATS at %s: %v", natsURL, err)
+		return nil, err
+	}
+
+	log.Printf("Successfully connected to NATS at %s", nc.ConnectedUrl())
+	return nc, nil
+}
